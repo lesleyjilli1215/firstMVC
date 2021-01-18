87f3fac6f0ca6719840372454a7a4c5ab285dbd7
Lesley Pan <lesley.jilli1215@gmail.com>
2021-01-18 17:34:00 +0800

    加入專案檔案。

---
+++ firstMVC/Models/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstMVC.Models
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
